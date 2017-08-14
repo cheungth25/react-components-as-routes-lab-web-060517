a3d25ad224937810d33e79a6f8e7e31f0f1ad09c
cheungth25 <cheungth25@gmail.com>
2017-08-14 11:57:52 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,23 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Actors Page </h1>
+        {actors.map((actor)=>{
+          return(
+            <div className='actor'>
+              <p> Actor Name: {actor.name} </p>
+              <ul>
+                {actor.movies.map((movie)=>{
+                  return(
+                    <li> {movie} </li>
+                  )
+                })
+                }
+              </ul>
+            </div>
+          )
+        })
+        }
     </div>
   );
 };
